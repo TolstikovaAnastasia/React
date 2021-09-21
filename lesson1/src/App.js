70ba3c9a198342da5abf92c4770ff86e5c3a553f
TolstikivaAnastasia <tolstikovaanastasia@gmail.com>
2021-09-21 22:36:30 +0200

    вроде сделала как разбирали на уроке

---
+++ lesson1/src/App.js
@@ -0,0 +1,14 @@
+import {Message} from "./components/Message";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Message>
+        <p></p>
+      </Message>
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson1/src/components/Message/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Message = () => {
+
+    return <p className="message">
+      'Hello, teacher!'
+    </p>
+  }
