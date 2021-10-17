3470fea29624f2b1bf16fd88bf627b67fc7a6f58
TolstikovaAnastasia <85073709+TolstikovaAnastasia@users.noreply.github.com>
2021-10-17 18:03:15 +0200

    Add files via upload
    обновила файл с учетом изменений в sendMessageForm с componentDidUpdate

--- lesson2-2/src/App.js
+++ lesson2-2/src/App.js
@@ -1,20 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { SendMessageForm } from "./components/SendMessageForm/sendMessageForm";
 import { MessageList } from "./components/MessageList/messageList";
 import './App.css';
 
 function App () {
-
-  let message = {
-    author: '',
-    text: ''
-  }
-
-  useEffect(() => {
-    if (message[message.author.length - 1] !== 'bot') {
-      return 'Ваше сообщение могут видеть все участники чата'
-    }
-  });
 
   return(
     <div className="App">
