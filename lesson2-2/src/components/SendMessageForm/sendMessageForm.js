7a0589aa5a8e8620adfa3af0150753da5dec1505
TolstikovaAnastasia <85073709+TolstikovaAnastasia@users.noreply.github.com>
2021-10-17 18:02:17 +0200

    Add files via upload
    реализован ответ бота с помощью componentDidUpdate

--- lesson2-2/src/components/SendMessageForm/sendMessageForm.js
+++ lesson2-2/src/components/SendMessageForm/sendMessageForm.js
@@ -6,7 +6,10 @@ export class SendMessageForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            message: ''
+            message: {
+                author: '',
+                text: ''
+            }
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -24,13 +27,24 @@ export class SendMessageForm extends React.Component {
         })
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.message !== this.state.message) {
+            return prevState.message;
+        } else if (prevProps.message.author !== 'bot') {
+            return this.message = {
+                author: 'bot',
+                text: 'Ваше сообщение могут видеть все участники чата'
+            };
+        }
+    }
+
     render(){
         return (
             <div className="sendMessageForm">
                 <form>
                     <input className="messageForm"
                         type="text" 
-                        value={this.state.message} 
+                        value={this.componentDidUpdate} 
                         onChange={this.handleChange} 
                         placeholder="Type message to send" />
                     <button className="buttonForm"
