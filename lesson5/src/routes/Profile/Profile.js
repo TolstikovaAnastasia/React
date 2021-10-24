b15f7091eb29a7ce11cdb34dac4fed72f32d485d
TolstikovaAnastasia <85073709+TolstikovaAnastasia@users.noreply.github.com>
2021-10-24 22:45:05 +0200

    Add files via upload

--- lesson5/src/routes/Profile/Profile.js
+++ lesson5/src/routes/Profile/Profile.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { ReduxProfile } from "../../redux/index.js";
+import { useSelector, useDispatch } from 'react-redux';
+import { PROFILE_CONTENT } from '../../redux/store/profile/actionTypes.js';
 
 export const Profile = (props) => {
+  const dispatch = useDispatch();
+  const content = useSelector((state) => {
+    return state.content;
+  });
   return (
     <div>
-      <ReduxProfile />
+      <input type="checkbox" value={content} onChange={() => {
+        dispatch({
+          type: PROFILE_CONTENT
+        })
+      }} />
     </div>
-  );
+  )
 };
