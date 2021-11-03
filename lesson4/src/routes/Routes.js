212dff4476d90ce90eb0abbdf34d14952bee67a7
TolstikovaAnastasia <85073709+TolstikovaAnastasia@users.noreply.github.com>
2021-11-03 21:13:49 +0200

    Add files via upload

--- lesson4/src/routes/Routes.js
+++ lesson4/src/routes/Routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Home } from "./Home/Home";
-import { Chats } from "./Chats/Chats";
-import { Chat } from "./Chats/routes/Chat/Chat";
+import { Chat } from "./Chats/chats";
+import { ChatPage } from "./Chats/routes/chatPage/chatPage";
 import { Profile } from "./Profile/Profile";
 import { NotFound } from "./NotFound/Error";
 import { getChatsByIdPath, getChatsPath } from "../navigation/Navigation";
@@ -12,8 +12,9 @@ export const Routes = (props) => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path={getChatsPath()} component={Chats} />
-      <Route path={getChatsByIdPath()} component={Chat} />
+      <Route exact path={getChatsPath()} component={Chat}>
+        <Route path={getChatsByIdPath()} component={ChatPage} />
+      </Route>
       <Route path="/profile" component={Profile} />
   
       <Route path="*" component={NotFound}/>
