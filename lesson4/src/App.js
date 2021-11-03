61d1ae3c29ba42f6dbfecdc36577812aba376b04
TolstikovaAnastasia <85073709+TolstikovaAnastasia@users.noreply.github.com>
2021-11-03 21:03:02 +0200

    Add files via upload

--- lesson4/src/App.js
+++ lesson4/src/App.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { SendMessageForm } from "./components/SendMessageForm/sendMessageForm";
-import { MessageList } from "./components/MessageList/messageList";
-import { ChatsPage } from './components/ChatContainer/ChatsPage/chatsPage.js';
-import { MessagePage } from './components/ChatContainer/MessagePage/messagePage.js';
 import { Header } from "./components/Header/Header";
 import { Routes } from "./routes/Routes";
 import { BrowserRouter } from 'react-router-dom';
@@ -15,10 +11,6 @@ function App () {
       <div className="App">
         <Header />
         <Routes />
-        <ChatsPage />
-        <MessagePage />
-        <MessageList />
-        <SendMessageForm />
       </div>
     </BrowserRouter>
   );
