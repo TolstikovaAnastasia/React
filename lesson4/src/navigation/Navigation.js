d3dfc98652828a26ea683a0684ea58a6ca7818b0
TolstikovaAnastasia <85073709+TolstikovaAnastasia@users.noreply.github.com>
2021-11-03 21:03:31 +0200

    Add files via upload

--- lesson4/src/navigation/Navigation.js
+++ lesson4/src/navigation/Navigation.js
@@ -4,6 +4,10 @@ export const getHomePath = () => '';
 
 export const getChatsPath = () => composePath(getHomePath(), 'chats');
 
-export const getChatsByIdPath = (chatId = ':chatId') => composePath(getChatsPath(), chatId);
+export const getChatsByIdPath = (chatId = ':chatId') => composePath(getChatsPath(), chatId, 'chatPage');
+
+export const getChatsMessagePageByIdPath = (chatId = ':chatId') => composePath(getChatsPath(), chatId, 'messagePage');
 
 export const getChat404Path = (chatId = ':chatId') => composePath(getChatsPath(), chatId, '404');
+
+export const getMessagePage404Path = (chatId = ':chatId') => composePath(getChatsPath(), chatId, '404');
